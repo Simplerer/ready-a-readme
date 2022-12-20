6f9642135ed080ca3f7a525a4522164e79efd882
A Stone <simplererstone@gmail.com>
2022-12-20 15:41:33 -0500

    set badges and honed README appearance, setting up utilities folder

---
+++ SamplerREADME.md
@@ -0,0 +1,35 @@
+# yoyoyo
+
+  ## [![License: GPL v3](https://img.shields.io/badge/License-GPL%20v3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
+  
+  ## Table of Contents
+  - [Description](#description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  
+  ## Description
+  yo
+  
+  ## Installation
+  oy
+  
+  ## Usage
+  oops
+
+  ## License
+  This application is covered under the GPLv3 licensing.
+  For information on what this license covers please visit this source [Choose a License](https://choosealicense.com/appendix/)
+  
+  ## Contributing
+  bloa
+  
+  ## Tests
+  ooob
+  
+  ## Questions
+  If you need any more information on using this application, my email is yo.
+  My GitHub profile can be located here - >  [oy](https://github.com/oy)  < -
--- index.js
+++ index.js
@@ -3,27 +3,25 @@ const fs = require('fs');
 const prompt = inquirer.createPromptModule();
 
 const populate = (readMe) =>{
-  fs.writeFileSync('READAME.md', readMe)
+  fs.writeFileSync('SamplerREADME.md', readMe)
 }
 
 const makeReadme = (answers) => {
+  let licenseBadge;
   if (answers.license === 'MIT') {
-    answers.license = `MIT
-    [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`}
+     licenseBadge = `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`}
   if (answers.license === 'GNU GPLv2') {
-    answers.license = `GNU GPLv2
-    [![License: GPL v2](https://img.shields.io/badge/License-GPL%20v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)`}
+    licenseBadge = `[![License: GPL v2](https://img.shields.io/badge/License-GPL%20v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)`}
   if (answers.license === 'Apache') {
-    answers.license = `Apache
-    [[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`}
-  if (answers.license === 'MIT') {
-    answers.license = `MIT
-    [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`}
-  if (answers.license === 'MIT') {
-    answers.license = `MIT
-    [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`}
+    licenseBadge = `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`}
+  if (answers.license === 'GPLv3') {
+    licenseBadge = `[![License: GPL v3](https://img.shields.io/badge/License-GPL%20v3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`}
+  if (answers.license === 'BSD3-clause') {
+    licenseBadge = `[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)`}
   
   return `# ${answers.projectname}
+
+  ## ${licenseBadge}
   
   ## Table of Contents
   - [Description](#description)
@@ -44,7 +42,8 @@ const makeReadme = (answers) => {
   ${answers.usage}
 
   ## License
-  ${answers.license}
+  This application is covered under the ${answers.license} licensing.
+  For information on what this license covers please visit this source [Choose a License](https://choosealicense.com/appendix/)
   
   ## Contributing
   ${answers.contributing}
@@ -54,7 +53,7 @@ const makeReadme = (answers) => {
   
   ## Questions
   If you need any more information on using this application, my email is ${answers.email}.
-  Refer to the repo here - >  ${answers.github}  < -`
+  My GitHub profile can be located here - >  [${answers.github}](https://github.com/${answers.github})  < -`
 }
 
 prompt([
